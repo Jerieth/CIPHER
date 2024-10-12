6ca59e06ebefa265ef044a3138601294893604b3
Jerieth <81718602+Jerieth@users.noreply.github.com>
2024-10-12 10:55:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ db_path = os.path.join(os.getcwd(), 'cypher.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cypher.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins=["http://ec2-3-81-104-212.compute-1.amazonaws.com:5000", "http://cipher.sctds.com"])
+socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins=["http://ec2-3-81-104-212.compute-1.amazonaws.com:5000", "http://cipher.sctds.com","https://voiceflow.com",r"^https:\/\/.*\.voiceflow\.com$"])
 
 # Define route for '/index.html'
 @app.route('/')
