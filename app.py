c9e8a56fb643bf3558cb1bae6bb6d5bc22a59db5
Jerieth <81718602+Jerieth@users.noreply.github.com>
2024-10-16 15:18:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY") or "a secret key"
 db_path = os.path.join(os.getcwd(), 'cypher.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Jerieth:G^EVA1b89lg-c5@localhost/cipher_mysql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Jerieth:G-LP1tHuNdErx5001aBtA@localhost/cipher_mysql'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins=["http://ec2-3-81-104-212.compute-1.amazonaws.com:5000", "107.180.4.7", r"^https:\/\/.*\.digitalmynds\.com$", "http://cipher.sctds.com","https://voiceflow.com",r"^https:\/\/.*\.voiceflow\.com$"])
